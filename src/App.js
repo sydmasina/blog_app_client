4b1755c25afb4881b7f4ed59bf071a98ecb87db4
sydmasina <masinasydwell@gmail.com>
2022-08-15 21:00:35 +0200

    create PostList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from  'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList'
 
 const App=  () =>{
     return <div className='container'>
         <h1 className='text-center'>Create post</h1>
         <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />
     </div>;
 }
 
